c1d802b1331e74eb89029227acf33fef1168dafa
qdxiaojunjun <164010934+qdxiaojunjun@users.noreply.github.com>
2024-08-29 20:40:37 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import Header from './components/Header';
 import Section from './components/Section';
 import Footer from './components/Footer';
 import './App.css';
+import About from './About';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header/>
         <Routes>
           <Route
             path="/"
             element={
               <>
-                <Section id="about" title="关于畅霄科技" content="畅霄科技专注于差旅服务..." />
+                <Section id="about" title="关于畅霄科技" content="畅霄科技专注于差旅服务..." to="/about"/>
                 <Section id="services" title="差旅服务" content="我们提供综合最优、低价优先..." />
                 <Section id="membership" title="会员权益" content="加入畅霄科技会员，享受我们的差旅服务..." />
                 <Section id="contact" title="联系我们" content="电话: 123-456-7890 | 邮箱: info@changxiao.com" />
@@ -26,6 +27,7 @@ function App() {
           />
           <Route path="/membership" element={<Membership />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </div>
